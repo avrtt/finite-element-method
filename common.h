41602a4a5a489186d2000252e7a1bff36f1412b1
Vladislav <lenferdetroud@gmail.com>
2021-11-01 15:49:23 +0700

    Create common.h

---
+++ common.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef COMMON_H
+#define COMMON_H
+
+typedef double real;
+void readParams();
+void readGrid();
+void config();
+//void globalAsm();
+//int factorization();
+//void lowGauss(real* in, real* out);
+//void upperGauss(real* in, real* out);
+//void multMatVect(real* in, real* out);
+//real multScalar(real* v1, real* v2);
+//void result();
+
+#endif
